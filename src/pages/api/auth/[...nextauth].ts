1025f3e73477978f04c8523648dbb7f151b59561
Filipe Moura <filipeasm18@gmail.com>
2022-03-02 22:25:55 -0300

    added nextauth secret

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,21 +1,22 @@
-import { query as q } from "faunadb"
+import { query as q } from 'faunadb'
 
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from 'next-auth'
+import GithubProvider from 'next-auth/providers/github'
 
 import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       authorization: {
         params: {
-          scope: 'read:user',
+          scope: 'read:user'
         }
       }
-    }),
+    })
   ],
   callbacks: {
     async session({ session }) {
@@ -26,7 +27,7 @@ export default NextAuth({
               q.Match(
                 q.Index('subscription_by_user_ref'),
                 q.Select(
-                  "ref",
+                  'ref',
                   q.Get(
                     q.Match(
                       q.Index('user_by_email'),
@@ -35,14 +36,11 @@ export default NextAuth({
                   )
                 )
               ),
-              q.Match(q.Index('subscription_by_status'),
-              "active"
-              )
+              q.Match(q.Index('subscription_by_status'), 'active')
             ])
           )
         )
-  
-  
+
         return {
           ...session,
           activeSubscription: userActiveSubscription
@@ -50,11 +48,11 @@ export default NextAuth({
       } catch {
         return {
           ...session,
-          activeSubscription: null,
+          activeSubscription: null
         }
       }
     },
-    async signIn({user, account, profile}) {
+    async signIn({ user, account, profile }) {
       const { email } = user
 
       try {
@@ -62,31 +60,17 @@ export default NextAuth({
           q.If(
             q.Not(
               q.Exists(
-                q.Match(
-                  q.Index('user_by_email'),
-                  q.Casefold(user.email)
-                )
+                q.Match(q.Index('user_by_email'), q.Casefold(user.email))
               )
             ),
-            q.Create(
-              q.Collection('users'),
-              { data: { email } }
-          ),
-          q.Get(
-            q.Match(
-              q.Index('user_by_email'),
-              q.Casefold(user.email)
-            )
-          )
+            q.Create(q.Collection('users'), { data: { email } }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(user.email)))
           )
         )
         return true
-
-
       } catch (error) {
         return false
       }
-
     }
   }
 })
